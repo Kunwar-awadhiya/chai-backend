6cc2b1b09c2c509aaf33d383bba07ca8d54596cc
Kunwar-awadhiya <kunwarawadhiya@gmail.com>
2025-09-13 15:11:35 +0530

    util code discussion

---
+++ vid_07/src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+// first method using Promise keyword 
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+
+
+// second method using hof
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
